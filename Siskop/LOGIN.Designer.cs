828430da9626acc7ebc922aa41dc257c1a0712fc
adelio-sumiya <adeliofriski28@gmail.com>
2025-05-28 19:24:24 +0700

    LOGIN

--- Siskop/LOGIN.Designer.cs
+++ Siskop/LOGIN.Designer.cs
@@ -35,12 +35,30 @@
             pictureBox4 = new PictureBox();
             pictureBox5 = new PictureBox();
             pictureBox6 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            pictureBox8 = new PictureBox();
+            pictureBox9 = new PictureBox();
+            pictureBox10 = new PictureBox();
+            pictureBox11 = new PictureBox();
+            pictureBox12 = new PictureBox();
+            pictureBox13 = new PictureBox();
+            pictureBox14 = new PictureBox();
+            username = new TextBox();
+            password = new TextBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox11).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox12).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox13).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox14).BeginInit();
             SuspendLayout();
             // 
             // pictureBox1
@@ -57,9 +75,9 @@
             // pictureBox2
             // 
             pictureBox2.BackgroundImage = (Image)resources.GetObject("pictureBox2.BackgroundImage");
-            pictureBox2.Location = new Point(395, 0);
+            pictureBox2.Location = new Point(374, 0);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(406, 451);
+            pictureBox2.Size = new Size(427, 451);
             pictureBox2.TabIndex = 1;
             pictureBox2.TabStop = false;
             // 
@@ -70,7 +88,7 @@
             pictureBox3.BackgroundImageLayout = ImageLayout.Zoom;
             pictureBox3.Location = new Point(30, 12);
             pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(731, 426);
+            pictureBox3.Size = new Size(746, 426);
             pictureBox3.TabIndex = 2;
             pictureBox3.TabStop = false;
             // 
@@ -79,7 +97,7 @@
             pictureBox4.BackColor = Color.Transparent;
             pictureBox4.BackgroundImage = (Image)resources.GetObject("pictureBox4.BackgroundImage");
             pictureBox4.BackgroundImageLayout = ImageLayout.Zoom;
-            pictureBox4.Location = new Point(110, 21);
+            pictureBox4.Location = new Point(110, 57);
             pictureBox4.Name = "pictureBox4";
             pictureBox4.Size = new Size(279, 269);
             pictureBox4.TabIndex = 3;
@@ -90,7 +108,7 @@
             pictureBox5.BackColor = Color.Transparent;
             pictureBox5.BackgroundImage = (Image)resources.GetObject("pictureBox5.BackgroundImage");
             pictureBox5.BackgroundImageLayout = ImageLayout.Zoom;
-            pictureBox5.Location = new Point(129, 321);
+            pictureBox5.Location = new Point(129, 357);
             pictureBox5.Name = "pictureBox5";
             pictureBox5.Size = new Size(239, 30);
             pictureBox5.TabIndex = 4;
@@ -101,17 +119,132 @@
             pictureBox6.BackColor = Color.Transparent;
             pictureBox6.BackgroundImage = (Image)resources.GetObject("pictureBox6.BackgroundImage");
             pictureBox6.BackgroundImageLayout = ImageLayout.Zoom;
-            pictureBox6.Location = new Point(177, 259);
+            pictureBox6.Location = new Point(177, 295);
             pictureBox6.Name = "pictureBox6";
             pictureBox6.Size = new Size(135, 56);
             pictureBox6.TabIndex = 5;
             pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.BackColor = Color.Transparent;
+            pictureBox7.BackgroundImage = (Image)resources.GetObject("pictureBox7.BackgroundImage");
+            pictureBox7.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox7.Location = new Point(374, 41);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(333, 386);
+            pictureBox7.TabIndex = 6;
+            pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.BackColor = Color.Transparent;
+            pictureBox8.BackgroundImage = (Image)resources.GetObject("pictureBox8.BackgroundImage");
+            pictureBox8.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox8.Location = new Point(492, 57);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(100, 50);
+            pictureBox8.TabIndex = 7;
+            pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            pictureBox9.BackColor = Color.Transparent;
+            pictureBox9.BackgroundImage = (Image)resources.GetObject("pictureBox9.BackgroundImage");
+            pictureBox9.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox9.Location = new Point(421, 136);
+            pictureBox9.Name = "pictureBox9";
+            pictureBox9.Size = new Size(98, 23);
+            pictureBox9.TabIndex = 8;
+            pictureBox9.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            pictureBox10.BackgroundImage = (Image)resources.GetObject("pictureBox10.BackgroundImage");
+            pictureBox10.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox10.Location = new Point(421, 165);
+            pictureBox10.Name = "pictureBox10";
+            pictureBox10.Size = new Size(238, 59);
+            pictureBox10.TabIndex = 9;
+            pictureBox10.TabStop = false;
+            // 
+            // pictureBox11
+            // 
+            pictureBox11.BackgroundImage = (Image)resources.GetObject("pictureBox11.BackgroundImage");
+            pictureBox11.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox11.Location = new Point(421, 273);
+            pictureBox11.Name = "pictureBox11";
+            pictureBox11.Size = new Size(238, 59);
+            pictureBox11.TabIndex = 10;
+            pictureBox11.TabStop = false;
+            // 
+            // pictureBox12
+            // 
+            pictureBox12.BackColor = Color.Transparent;
+            pictureBox12.BackgroundImage = (Image)resources.GetObject("pictureBox12.BackgroundImage");
+            pictureBox12.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox12.Location = new Point(421, 244);
+            pictureBox12.Name = "pictureBox12";
+            pictureBox12.Size = new Size(99, 23);
+            pictureBox12.TabIndex = 11;
+            pictureBox12.TabStop = false;
+            // 
+            // pictureBox13
+            // 
+            pictureBox13.BackColor = Color.Transparent;
+            pictureBox13.BackgroundImage = (Image)resources.GetObject("pictureBox13.BackgroundImage");
+            pictureBox13.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox13.Location = new Point(460, 338);
+            pictureBox13.Name = "pictureBox13";
+            pictureBox13.Size = new Size(173, 69);
+            pictureBox13.TabIndex = 12;
+            pictureBox13.TabStop = false;
+            // 
+            // pictureBox14
+            // 
+            pictureBox14.BackgroundImage = (Image)resources.GetObject("pictureBox14.BackgroundImage");
+            pictureBox14.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox14.Location = new Point(481, 357);
+            pictureBox14.Name = "pictureBox14";
+            pictureBox14.Size = new Size(123, 30);
+            pictureBox14.TabIndex = 13;
+            pictureBox14.TabStop = false;
+            // 
+            // username
+            // 
+            username.BackColor = Color.White;
+            username.BorderStyle = BorderStyle.None;
+            username.ForeColor = Color.Transparent;
+            username.Location = new Point(436, 184);
+            username.Name = "username";
+            username.Size = new Size(207, 16);
+            username.TabIndex = 14;
+            // 
+            // password
+            // 
+            password.BackColor = Color.White;
+            password.BorderStyle = BorderStyle.None;
+            password.ForeColor = Color.Transparent;
+            password.Location = new Point(436, 291);
+            password.Name = "password";
+            password.Size = new Size(207, 16);
+            password.TabIndex = 15;
             // 
             // LOGIN
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(username);
+            Controls.Add(password);
+            Controls.Add(pictureBox14);
+            Controls.Add(pictureBox13);
+            Controls.Add(pictureBox12);
+            Controls.Add(pictureBox11);
+            Controls.Add(pictureBox10);
+            Controls.Add(pictureBox9);
+            Controls.Add(pictureBox8);
+            Controls.Add(pictureBox7);
             Controls.Add(pictureBox6);
             Controls.Add(pictureBox5);
             Controls.Add(pictureBox4);
@@ -126,7 +259,16 @@
             ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox11).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox12).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox13).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox14).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -137,5 +279,15 @@
         private PictureBox pictureBox4;
         private PictureBox pictureBox5;
         private PictureBox pictureBox6;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox8;
+        private PictureBox pictureBox9;
+        private PictureBox pictureBox10;
+        private PictureBox pictureBox11;
+        private PictureBox pictureBox12;
+        private PictureBox pictureBox13;
+        private PictureBox pictureBox14;
+        private TextBox username;
+        private TextBox password;
     }
 }
